1d08f6fe5822128b52a309d9c0bdd03d1051823e
Yamzee <a10wn@mail.ru>
2018-12-22 23:34:02 -0600

    new file:   ArchLabs/.Xresources 	new file:   ArchLabs/.config/.composite_enabled 	new file:   ArchLabs/.config/QtProject.conf 	new file:   ArchLabs/.config/autostart/discord-stable.desktop 	new file:   ArchLabs/.config/bspwm/bspwmrc 	new file:   ArchLabs/.config/cmus/autosave 	new file:   ArchLabs/.config/cmus/cache 	new file:   ArchLabs/.config/cmus/command-history 	new file:   ArchLabs/.config/cmus/lib.pl 	new file:   ArchLabs/.config/cmus/playlist.pl 	new file:   ArchLabs/.config/cmus/search-history 	new file:   ArchLabs/.config/compton.conf 	new file:   ArchLabs/.config/conky/ArchLabs.conkyrc 	new file:   ArchLabs/.config/dconf/user 	new file:   ArchLabs/.config/dunst/dunstrc 	new file:   ArchLabs/.config/fontconfig/fonts.conf 	new file:   ArchLabs/.config/gconf/desktop/%gconf.xml 	new file:   ArchLabs/.config/gconf/desktop/gnome/%gconf.xml 	new file:   ArchLabs/.config/gconf/desktop/gnome/url-handlers/%gconf.xml 	new file:   ArchLabs/.config/gconf/desktop/gnome/url-handlers/mailto/%gconf.xml 	new file:   ArchLabs/.config/geany/colorschemes/archlabs-dARK.conf 	new file:   ArchLabs/.config/geany/colorschemes/archlabs-dark.conf 	new file:   ArchLabs/.config/geany/colorschemes/archlabs-light.conf 	new file:   ArchLabs/.config/geany/geany.conf 	new file:   ArchLabs/.config/gpicview/gpicview.conf 	new file:   ArchLabs/.config/gtk-2.0/gtkfilechooser.ini 	new file:   ArchLabs/.config/gtk-3.0/settings.ini 	new file:   ArchLabs/.config/htop/htoprc 	new file:   ArchLabs/.config/i3/config 	new file:   ArchLabs/.config/jgmenu/append.csv 	new file:   ArchLabs/.config/jgmenu/jgmenurc 	new file:   ArchLabs/.config/jgmenu/prepend.csv 	new file:   ArchLabs/.config/libfm/libfm.conf 	new file:   ArchLabs/.config/mimeapps.list 	new file:   ArchLabs/.config/mpd/mpd.conf 	new file:   ArchLabs/.config/networkmanager-dmenu/config.ini 	new file:   ArchLabs/.config/nitrogen/bg-saved.cfg 	new file:   ArchLabs/.config/nitrogen/nitrogen.cfg 	new file:   ArchLabs/.config/nvim/.netrwhist 	new file:   ArchLabs/.config/nvim/colors/jinx.vim 	new file:   ArchLabs/.config/nvim/init.vim 	new file:   ArchLabs/.config/openbox/autostart 	new file:   ArchLabs/.config/openbox/environment 	new file:   ArchLabs/.config/openbox/menu.xml 	new file:   ArchLabs/.config/openbox/rc.xml 	new file:   ArchLabs/.config/pavucontrol.ini 	new file:   ArchLabs/.config/polybar/config 	new file:   ArchLabs/.config/polybar/master.conf 	new file:   ArchLabs/.config/polybar/modules.conf 	new file:   ArchLabs/.config/polybar/sessions/bspwm-sessionfile 	new file:   ArchLabs/.config/polybar/sessions/i3-sessionfile 	new file:   ArchLabs/.config/polybar/sessions/openbox-sessionfile 	new file:   ArchLabs/.config/polybar/sessions/sessionfile 	new file:   ArchLabs/.config/pulse/c76c8a8a9cbe4d4d8d182b889b328955-card-database.tdb 	new file:   ArchLabs/.config/pulse/c76c8a8a9cbe4d4d8d182b889b328955-default-sink 	new file:   ArchLabs/.config/pulse/c76c8a8a9cbe4d4d8d182b889b328955-default-source 	new file:   ArchLabs/.config/pulse/c76c8a8a9cbe4d4d8d182b889b328955-device-volumes.tdb 	new file:   ArchLabs/.config/pulse/c76c8a8a9cbe4d4d8d182b889b328955-stream-volumes.tdb 	new file:   ArchLabs/.config/pulse/cookie 	new file:   ArchLabs/.config/qt5ct/qt5ct.conf 	new file:   ArchLabs/.config/ranger/colorschemes/__init__.py 	new file:   ArchLabs/.config/ranger/colorschemes/jinx.py 	new file:   ArchLabs/.config/ranger/rc.conf 	new file:   ArchLabs/.config/ranger/rifle.conf 	new file:   ArchLabs/.config/ranger/scope.sh 	new file:   ArchLabs/.config/skippy-xd/skippy-xd.rc 	new file:   ArchLabs/.config/sxhkd/sxhkdrc 	new file:   ArchLabs/.config/tint2/tint2rc 	new file:   ArchLabs/.ncmpcpp/config 	new file:   ArchLabs/.ncmpcpp/error.log 	new file:   ArchLabs/.zshrc 	new file:   README

---
+++ ArchLabs/.config/cmus/lib.pl
@@ -0,0 +1,40 @@
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 01 Intro.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 02 Numbnuts.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 03 Fresh Out.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 04 Jelly Donuts.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 05 400 Cherries Per Minute.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 06 Shake Your Tail.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 07 Crush Everyone.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 08 Back Up.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 09 Here We Go.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 10 Coward Killing Time.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 11 Break.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 12 Big Money.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 13 Sugarkill X.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 14 Dragon Whips His Tail.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 15 Please.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 16 Brain Specialist.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 17 Snort A Lot Of Crack.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 18 Listen To My Shit.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 19 Play This In Audiosurf.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 20 Dino Rampage.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 21 Knocked The Fuck Out.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/The Quick Brown Fox/The Quick Brown Fox - The Quick Brown Fox - 22 I'm A Fucking Car.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/BRUTAL RHTHM RIDER/The Quick Brown Fox - BRUTAL RHYTHM RIDER - 01 THOROUGH PIÑATA DICKING AMATEUR XXX.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/BRUTAL RHTHM RIDER/The Quick Brown Fox - BRUTAL RHYTHM RIDER - 02 DAS LOVE BOP.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/BRUTAL RHTHM RIDER/The Quick Brown Fox - BRUTAL RHYTHM RIDER - 03 CRUISE BEAST.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/BRUTAL RHTHM RIDER/The Quick Brown Fox - BRUTAL RHYTHM RIDER - 04 LOUDER!! LOUDER!!!! LOUDER!!!!!!.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/BRUTAL RHTHM RIDER/The Quick Brown Fox - BRUTAL RHYTHM RIDER - 05 HIT AND RUN IN 1920 A.D..mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/BRUTAL RHTHM RIDER/The Quick Brown Fox - BRUTAL RHYTHM RIDER - 06 LIKE A G666.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/BRUTAL RHTHM RIDER/The Quick Brown Fox - BRUTAL RHYTHM RIDER - 07 YEAR OF THE BAD DRAGON.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/BRUTAL RHTHM RIDER/The Quick Brown Fox - BRUTAL RHYTHM RIDER - 08 BUB VS BOB.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/BRUTAL RHTHM RIDER/The Quick Brown Fox - BRUTAL RHYTHM RIDER - 09 NO HESITATION.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/Wanderlust/The Quick Brown Fox - WANDERLUST - 01 LIFELINE.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/Wanderlust/The Quick Brown Fox - WANDERLUST - 02 WANDERLUST.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/Wanderlust/The Quick Brown Fox - WANDERLUST - 03 BAD DREAM BABY.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/Wanderlust/The Quick Brown Fox - WANDERLUST - 04 VIVID.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/Wanderlust/The Quick Brown Fox - WANDERLUST - 05 CRT.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/Wanderlust/The Quick Brown Fox - WANDERLUST - 06 LINE OUT.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/Wanderlust/The Quick Brown Fox - WANDERLUST - 07 晴天の霹靂.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/Wanderlust/The Quick Brown Fox - WANDERLUST - 08 AUTUMN DAY DISTRACTION.mp3
+/mnt/ssd1/Users/a10wn/Music/Lapfox/The Quick Brown Fox/Wanderlust/The Quick Brown Fox - WANDERLUST - 09 JUST HESITATION.mp3
